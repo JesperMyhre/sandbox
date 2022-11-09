c222e0862ba2dc5a5abefd571e92c232860b6f43
JesperMyhre <jesper.ovreness@gmail.com>
2022-11-09 22:54:56 +0100

    små endringer

--- DELEKSAMEN-2/javascript/scripts.js
+++ DELEKSAMEN-2/javascript/scripts.js
@@ -20,3 +20,21 @@ const navMenu = () => {
 }
 
 console.log(menu);
+
+//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+let mybutton = document.getElementById("toppknapp");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
