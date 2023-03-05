edccfde5f3d8057497a88975a662bf0da228e723
JesperMyhre <jesper.ovreness@gmail.com>
2023-03-05 15:04:22 +0100

    ja

--- DELEKSAMEN-2/javascript/scripts.js
+++ DELEKSAMEN-2/javascript/scripts.js
@@ -1,13 +1,3 @@
-//https://replit.com/talk/learn/Add-sound-to-a-button-in-Html-and-JavaScript/120876
-const audio = new Audio("../javascript/sound.mp3");
-const buttons = document.querySelectorAll("sound-button");
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    audio.play();
-  });
-});
-
 const menu = document.getElementById("nav-menu");
 
 const navMenu = () => {
